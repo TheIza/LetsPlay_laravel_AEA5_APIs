633d649b80fcbb98e185c040d20c1b66123f4366
izarbe <izarbelanuza06@gmail.com>
2026-05-01 01:40:10 +0200

    xd

--- app/Models/Videojuego.php
+++ app/Models/Videojuego.php
@@ -7,14 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 class Videojuego extends Model
 {
     protected $fillable = [
-        'nombre',
-        'genero',
-        'plataforma',
-        'fecha_lanzamiento',
-        'precio',
-        'imagen_url',
-        'stock'
-    ];
+    'nombre',
+    'genero',
+    'precio',
+    'stock',
+    'imagen_url'
+];
 
    
 }
--- routes/api.php
+++ routes/api.php
@@ -1,15 +1,9 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\VideojuegoController;
-use App\Models\Videojuego;
 
-
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
-Route::apiResource('videojuegos', VideojuegoController::class);
-Route::get('/videojuegos/{id}', [VideojuegoController::class, 'show']);
-Route::post('/videojuegos', [VideojuegoController::class, 'store']);
+Route::apiResource('videojuego', VideojuegoController::class);
+Route::get('/test', function () {
+    return 'API FUNCIONA';
+});
